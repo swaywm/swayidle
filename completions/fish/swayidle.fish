844dfde8538c1f55aaf254c18649d419bdff7a92
Gaël PORTAY <gael.portay@collabora.com>
2019-08-26 20:27:42 -0400

    Update bash and fish completions with option -w

--- completions/fish/swayidle.fish
+++ completions/fish/swayidle.fish
@@ -1,3 +1,4 @@
 # swayidle
 complete -c swayidle -s h --description 'show help'
 complete -c swayidle -s d --description 'debug'
+complete -c swayidle -s w --description 'wait for command to finish'
