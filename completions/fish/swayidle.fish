e25606f9234e925f32c755515272095a1b48c4c8
Jason Nader <jason.nader@protonmail.com>
2020-03-02 20:11:34 +0900

    Update fish completions with `events`

--- completions/fish/swayidle.fish
+++ completions/fish/swayidle.fish
@@ -1,4 +1,12 @@
 # swayidle
+set -l all_events timeout before-sleep after-resume lock unlock idlehint
+set -l cmd_events before-sleep after-resume lock unlock
+set -l time_events idlehint timeout
+
+complete -c swayidle --arguments "$all_events"
+complete -c swayidle --condition "__fish_seen_subcommand_from $cmd_events" --require-parameter
+complete -c swayidle --condition "__fish_seen_subcommand_from $time_events" --exclusive
+
 complete -c swayidle -s h --description 'show help'
 complete -c swayidle -s d --description 'debug'
 complete -c swayidle -s w --description 'wait for command to finish'
